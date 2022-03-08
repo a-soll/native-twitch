a2bdfcc910c838bd44fa7767293c45194cb4f6b5
Adam Solloway <ajsolloway@gmail.com>
2022-03-07 21:02:55 -0500

    Initial Commit

---
+++ native_twitch/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  native_twitch
+//
+//  Created by Adam Solloway on 3/7/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ native_twitch/native_twitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  native_twitchApp.swift
+//  native_twitch
+//
+//  Created by Adam Solloway on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct native_twitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
