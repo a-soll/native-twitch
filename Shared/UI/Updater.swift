14439b3b366cd223d31620ae07b799de330e1167
Adam Solloway <ajsolloway@gmail.com>
2022-08-29 02:49:40 -0400

    test auto update

--- Shared/UI/Updater.swift
+++ Shared/UI/Updater.swift
@@ -32,7 +32,7 @@ final class UpdaterViewModel: ObservableObject {
 // See https://stackoverflow.com/questions/68553092/menu-not-updating-swiftui-bug for more information
 struct CheckForUpdatesView: View {
     @ObservedObject var updaterViewModel: UpdaterViewModel
-    
+
     var body: some View {
         Button("Check for Updates…", action: updaterViewModel.checkForUpdates)
             .disabled(!updaterViewModel.canCheckForUpdates)
